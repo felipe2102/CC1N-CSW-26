23eb7840d0061dfb181fa867bdff628635ef6673
Felipe Ribeiro <feliperb.br@gmail.com>
2026-03-30 10:33:45 -0300

    Exercício 1j

---
+++ 1 periodo/fundamentos/trabalho/1j.py
@@ -0,0 +1,16 @@
+import math
+
+try:
+    raio = int(input("Insira o valor do raio da caixa d'água: "))
+    altura = int(input("Insira a altura da caixa d'água: "))
+except Exception as erro:
+    print(f'Um erro ocorreu! Código de erro: {erro}')
+
+try:
+    volume = 2*math.pi*raio*(raio + altura)
+    volume = volume * 1000
+    autonomia = volume / 1350
+
+    print(f"A autonomia da caixa d'água é de {autonomia:.1f} horas")
+except Exception as erro:
+    print(f'Um erro ocorreu! Código de erro: {erro}')
