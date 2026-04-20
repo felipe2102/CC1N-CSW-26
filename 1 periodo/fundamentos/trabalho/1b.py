6d43bb369fdd6ab83c86b345a26cdea7a0821da6
Felipe Ribeiro <feliperb.br@gmail.com>
2026-04-20 08:32:47 -0300

    a

--- 1 periodo/fundamentos/trabalho/1b.py
+++
@@ -1,7 +0,0 @@
-try:
-    lado = int(input("Digite o valor do lado do quadrado: "))
-except Exception as erro:
-    print(f'\nUm erro ocorreu! Mensagem de erro: {erro}')
-
-area = lado*lado
-print(f'A área do quadrado é {area}')
--- 1 periodo/fundamentos/trabalho/1c.py
+++
@@ -1,9 +0,0 @@
-try:
-    lado = float(input("Insira o valor do lado do retângulo:"))
-    altura = float(input("Insira o valor da altura do retângulo:"))
-except Exception as erro:
-    print(f'\nUm erro ocorreu! Mensagem de erro: {erro}')
-
-area = lado*altura
-
-print(f'A área do retângulo de lado {lado} e altura {altura} é {area}')
