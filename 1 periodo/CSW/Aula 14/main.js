0adf910d19f5b77b4e3e1da7d8ba2f36efa0ed2a
Daniel Fragoso Pimentel de Almeida <danielfragoso048@gmail.com>
2026-05-20 20:43:39 -0300

    Aula 14

--- 1 periodo/CSW/Aula 14/main.js
+++ 1 periodo/CSW/Aula 14/main.js
@@ -1,7 +1,20 @@
-document.addEventListener('keypress', break_system);
-
-function break_system() {
-    while(true) {
-        console.log('a');
+for(let i=0; i<=10; i++) {
+    if(i%2 == 0) {
+        console.log(i);
     }
+}
+var i = 0;
+while (i <= 10) {
+    if (i%2 != 0) {
+        console.log(i);
+    }
+    i++;
+}
+
+
+function get_age() {
+    let nome = document.getElementById("input").value;
+
+    document.getElementById("title").innerHTML = "Você"
+
 }
